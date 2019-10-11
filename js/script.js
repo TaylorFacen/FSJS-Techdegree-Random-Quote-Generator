673886cb27d4c65c6c7d9f49d25f91e4056bafcf
Taylor Facen <taylor.facen@gmail.com>
2019-10-10 22:59:53 -0400

    Added quotes array

--- js/script.js
+++ js/script.js
@@ -5,15 +5,38 @@ project 1 - A Random Quote Generator
 
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+var quotes = [
+  {
+    quote: "While we may not always have control over our options, we always have control over how we choose among them.",
+    "source": "Greg McKeown",
+    "citation": "Essentialism",
+    "year": 2014
+  },
+  {
+    quote: "Mindfulness is simply the art of deliberately doing on ething at a time.",
+    "source": "Chris Baily",
+    "citation": "The Productivity Project",
+    "year": 2016
+  },
+  {
+    quote: "We must have faith in God's timing, especially when it's scary. We must have faith in the process.",
+    "source": "Valeisha Butterfield Jones",
+    "citation": "The Girlprint",
+    "year": 2014
+  },
+  {
+    quote: "Our thoughts become our words, out words become our beliefs, our beliefs become our actions, our actions become our habits, and our habits beccome our realities.",
+    "source": "Jen Sincero",
+    "citation": "You are a Badass",
+    "year": 2013
+  },
+  {
+    quote: "Everyone on earth was carrying around an unseen history, and that alone deserves some tolerance.",
+    "source": "Michelle Obama",
+    "citation": "Becoming",
+    "year": 2018
+  }
+]
 
 
 
